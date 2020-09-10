f36b84d0c5abb5a416a81d12aed8c8daba12378c
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-09-11 01:27:08 +0200

    Fix build with cortex-m master

--- src/lib.rs
+++ src/lib.rs
@@ -223,9 +223,10 @@ pub enum Interrupt {
     #[doc = "38 - FPU"]
     FPU = 38,
 }
-unsafe impl bare_metal::Nr for Interrupt {
+
+unsafe impl cortex_m::interrupt::InterruptNumber for Interrupt {
     #[inline(always)]
-    fn nr(&self) -> u8 {
+    fn number(self) -> u16 {
         *self as u8
     }
 }
